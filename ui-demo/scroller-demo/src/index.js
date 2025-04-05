299b899320d4d02d2096c20acce109ed50a142d5
huanghai21 <huanghai_l@126.com>
2025-04-05 20:20:15 +0800

    在 package.json 中添加 vconsole 依赖，并在 index.js 中初始化 vConsole，便于调试和日志记录。

--- ui-demo/scroller-demo/src/index.js
+++ ui-demo/scroller-demo/src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import VConsole from 'vconsole';
+
+// 直接初始化
+const vConsole = new VConsole();
+console.log('vConsole 已加载');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
